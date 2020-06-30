814df3de526129c73e57ae9c1440333f397b121c
Kyle Wiseman <kwiseman@highpoint.edu>
2020-06-30 13:51:57 -0400

    Added fuctionality to display posts in basic html

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Perseus'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author' : {'username': 'Sarah'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user,posts=posts)
