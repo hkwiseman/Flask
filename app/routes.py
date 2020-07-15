c68058c68a49898d53ed31df6b46a97677f5ebdf
Kyle Wiseman <kwiseman@highpoint.edu>
2020-07-15 14:24:41 -0400

    Added explore page to see all posts globally and ability to submit posts as user

--- app/routes.py
+++ app/routes.py
@@ -11,22 +11,24 @@ from app.forms import RegistrationForm
 from datetime import datetime
 from app.forms import EditProfileForm
 from app.forms import EmptyForm
-@app.route('/')
-@app.route('/index')
+from app.forms import PostForm
+from app.models import Post
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    user = {'username': 'Perseus'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author' : {'username': 'Sarah'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home',posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    posts = current_user.followed_posts().all()
+    return render_template('index.html', title='Home',form=form,
+                            posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -134,3 +136,10 @@ def unfollow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
+
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
+
